8ffdd16c74b7ccfc25975363b5f85ff26a22bd69
DoHe <wahlholz@gmx.de>
2017-03-10 21:55:31 +0100

    Cleanup index.js

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const connect = require('connect');
 const serveStatic = require('serve-static');
 
 const port = process.env.PORT || 5000;
-connect().use(serveStatic('static')).listen(port, function(){
-    console.log('Server running on ' + port + '...');
-});
+const server = connect().use(serveStatic('static'))
+server.listen(port, () => console.log('Server running on ' + port + '...'));
