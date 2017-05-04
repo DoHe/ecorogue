2d3f9e8be7607dc089480b2d25bca51bdd8358db
DoHe <wahlholz@gmx.de>
2017-05-04 19:11:54 +0200

    Add adventure

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const connect = require('connect');
 const serveStatic = require('serve-static');
 
 const port = process.env.PORT || 5000;
-const server = connect().use(serveStatic('static'))
-server.listen(port, () => console.log('Server running on ' + port + '...'));
+const server = connect().use(serveStatic('static'));
+server.listen(port, () => console.log(`Server running on ${port}...`));
